01eee40c81fcee52985385daaa83f958d474e9ed
Lorenzo Bolla <code@lbolla.info>
2020-12-08 20:26:04 +0100

    Install passff-host to get passwords in FF

--- bootstrap.d/apps.sh
+++ bootstrap.d/apps.sh
@@ -2,6 +2,7 @@ source bootstrap.d/aws.sh
 source bootstrap.d/emacs.sh
 source bootstrap.d/fzf.sh
 source bootstrap.d/git.sh
+source bootstrap.d/passff-host.sh
 source bootstrap.d/rclone.sh
 source bootstrap.d/restic.sh
 source bootstrap.d/rg.sh
---
+++ bootstrap.d/passff-host.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# See https://github.com/passff/passff-host
+
+PASSFF_EXEC=/home/lbolla/.mozilla/native-messaging-hosts/passff.py
+if [ ! -x ${PASSFF_EXEC} ]; then
+    curl -sSL github.com/passff/passff-host/releases/latest/download/install_host_app.sh | bash -s -- firefox
+fi
