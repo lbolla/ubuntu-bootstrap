9d1a0d6b1be3d3e0cafbfbf0362efeea654a20b8
Lorenzo Bolla <code@lbolla.info>
2020-12-08 16:37:59 +0100

    Install fonts

---
+++ bootstrap.d/fonts.sh
@@ -0,0 +1,18 @@
+info 'Installing fonts'
+
+debug 'Installing Iosevka'
+
+# See https://github.com/be5invis/Iosevka/releases
+IOSEVKA_D=/usr/share/fonts/truetype/iosevka
+if [ ! -d ${IOSEVKA_D} ]; then
+    VERSION=4.0.2
+    NAME=ttf-iosevka-${VERSION}.zip
+
+    if [ ! -f /tmp/${NAME} ]; then
+        curl -L https://github.com/be5invis/Iosevka/releases/download/v${VERSION}/${NAME} -o /tmp/${NAME}
+    fi
+
+    sudo mkdir -p ${IOSEVKA_D}
+    sudo unzip /tmp/${NAME} -d ${IOSEVKA_D}
+    sudo fc-cache
+fi
