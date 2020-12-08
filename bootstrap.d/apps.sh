257d432486e64b1bc20b0970a3e4dd8eb4770a0a
Lorenzo Bolla <code@lbolla.info>
2020-12-08 12:59:01 +0100

    Install aws

--- bootstrap.d/apps.sh
+++ bootstrap.d/apps.sh
@@ -1,8 +1,9 @@
+source bootstrap.d/aws.sh
 source bootstrap.d/fzf.sh
 source bootstrap.d/git.sh
 source bootstrap.d/rclone.sh
 source bootstrap.d/restic.sh
 source bootstrap.d/rg.sh
-# TODO aws
-# TODO emacs
 source bootstrap.d/vcs-resolve.sh
+
+# TODO emacs
---
+++ bootstrap.d/aws.sh
@@ -0,0 +1,11 @@
+info 'Install aws'
+
+# See https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html#cliv2-linux-install
+
+if ! command -v aws &> /dev/null; then
+    pushd /tmp
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    popd
+fi
