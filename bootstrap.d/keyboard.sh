96f9e55310fdd550512ddfaa7c36748a2ce2560f
Lorenzo Bolla <code@lbolla.info>
2020-12-16 15:40:24 +0100

    Fix keyboard script

--- bootstrap.d/keyboard.sh
+++ bootstrap.d/keyboard.sh
@@ -2,7 +2,7 @@
 
 info 'Setup keyboard'
 
-if [ $(grep -q 'XKBOPTIONS=""' /etc/default/keyboard) ]; then
+if grep -q 'XKBOPTIONS=""' /etc/default/keyboard; then
     sudo sed -i 's/XKBOPTIONS=""/XKBOPTIONS="ctrl:nocaps"/' /etc/default/keyboard
     setxkbmap -option "ctrl:nocaps"
 fi
