56fa6d47abf753693d944fc0af30a98539cef4a7
Lorenzo Bolla <code@lbolla.info>
2021-03-09 13:39:47 +0100

    Install quasi-monochrome theme

--- bootstrap.d/emacs.sh
+++ bootstrap.d/emacs.sh
@@ -13,3 +13,11 @@ if [ "$_bootstrap_upgrade" -eq 1 ]; then
     git pull
     popd
 fi
+
+EMACS_QUASI_MONOCHROME_D=${HOME}/src/github.com/lbolla/emacs-quasi-monochrome
+
+if [ ! -d "${EMACS_QUASI_MONOCHROME_D}" ]; then
+    info 'Install quasi-monochrome theme'
+    git clone https://github.com/lbolla/emacs-quasi-monochrome.git "${EMACS_QUASI_MONOCHROME_D}"
+fi
+
