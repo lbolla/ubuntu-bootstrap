c7c9f53f42ae22c07948a92845cca12312acc183
Lorenzo Bolla <code@lbolla.info>
2021-03-31 10:05:38 +0200

    fzf-tab: do as zsh-autosuggestions

--- bootstrap.d/zsh.sh
+++ bootstrap.d/zsh.sh
@@ -30,7 +30,8 @@ fi
 
 debug 'Install fzf-tab'
 # See https://github.com/Aloxaf/fzf-tab
-FZF_TAB_D=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+FZF_TAB_D=${HOME}/src/github.com/Aloxaf/fzf-tab/
 if [ ! -d "${FZF_TAB_D}" ]; then
     git clone https://github.com/Aloxaf/fzf-tab ${FZF_TAB_D}
+    ln -sf "${ZSH_AUTOSUGGESTIONS_D}" "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/fzf-tab"
 fi
