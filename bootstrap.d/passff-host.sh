566477c039433cc38be87b286c8cdd2d7380b68a
Lorenzo Bolla <code@lbolla.info>
2021-04-19 17:25:58 +0200

    pass: remove mention to home

--- bootstrap.d/passff-host.sh
+++ bootstrap.d/passff-host.sh
@@ -4,7 +4,7 @@ info 'Install passff-host'
 
 # See https://github.com/passff/passff-host
 
-PASSFF_EXEC=/home/lbolla/.mozilla/native-messaging-hosts/passff.py
+PASSFF_EXEC=$HOME/.mozilla/native-messaging-hosts/passff.py
 if [ ! -x "${PASSFF_EXEC}" ] || [ "$_bootstrap_upgrade" -eq 1 ]; then
     curl -sSL github.com/passff/passff-host/releases/latest/download/install_host_app.sh | bash -s -- firefox
 fi
